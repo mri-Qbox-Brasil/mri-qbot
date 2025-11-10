c30466f448610027ce430b0df07b9fbee51c039e
SubZer0GLX <subzeropacker@hotmail.com>
2025-11-09 22:39:22 -0300

    feat: add gemini moderator command and worker utils.

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,9 +1,11 @@
 const inviteBlocker = require('../commands/blockers/inviteblocker');
+const geminiModerator = require('../commands/blockers/geminiModerator');
 
 module.exports = {
     name: 'messageCreate',
     once: false,
     async execute(message) {
         await inviteBlocker.onMessageCreate(message);
+        await geminiModerator.onMessageCreate(message);
     },
 };
